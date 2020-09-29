c400e9a5050b4b8bea4ddb240bc7f66867df5e6e
Brian McPhillips <B.McPhillips89@gmail.com>
2020-09-29 00:04:32 -0700

    user login token attached

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/user-service');
+const { profile } = require('console');
 
 describe('Tardygram routes', () => {
   beforeEach(() => {
@@ -23,6 +25,30 @@ describe('Tardygram routes', () => {
       id: expect.any(String),
       email: 'test@test.com',
       profilePhotoUrl: 'https://www.placecage.com/200/200'
+    });
+  });
+
+  it('logs in a user via POST', async() => {
+    const user = await UserService.create({ 
+      email: 'test@test.com', 
+      password: 'password',
+      profilePhotoUrl: 'https://www.placecage.com/200/200'
+    });
+
+    const response = await request(app)
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'test@test.com',
+        password: 'password',
+        profilePhotoUrl: 'https://www.placecage.com/200/200'
+        
+      });
+
+    expect(response.body).toEqual({
+      id: user.id,
+      email: 'test@test.com',
+      profilePhotoUrl: 'https://www.placecage.com/200/200'
+      
     });
   });
 });
