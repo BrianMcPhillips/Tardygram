271b602b1bebf2fe08cd3feee5a803dbacea81b5
Brian McPhillips <B.McPhillips89@gmail.com>
2020-09-28 23:01:58 -0700

    create test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,6 +5,24 @@ const app = require('../lib/app');
 
 describe('Tardygram routes', () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+
+  it('signs up a user via POST', async() => {
+    const response = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'test@test.com',
+        password: 'password',
+        profilePhotoUrl: 'https://www.placecage.com/200/200'
+      });
+
+    
+    
+    expect(response.body).toEqual({
+      id: expect.any(String),
+      email: 'test@test.com',
+      profilePhotoUrl: 'https://www.placecage.com/200/200'
+    });
   });
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -4,5 +4,14 @@ CREATE TABLE users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     email TEXT NOT NULL,
     password_hash TEXT NOT NULL,
-    profile_photo_url 
+    profile_photo_url TEXT NOT NULL
+    
 );
+
+-- CREATE TABLE posts (
+--     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+--     user_id BIGINT REFERNCES users(id),
+--     photo_url TEXT NOT NULL,
+--     caption TEXT NOT NULL,
+--     tags TEXT[]
+-- )
